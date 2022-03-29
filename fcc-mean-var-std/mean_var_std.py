1da5f714b5694d72ddbe31683b8fe28d7380ff46
mexudi <soufianmexudi20@gmail.com>
2022-03-29 22:45:01 +0000

    Version with less code

--- fcc-mean-var-std/mean_var_std.py
+++ fcc-mean-var-std/mean_var_std.py
@@ -6,12 +6,12 @@ def calculate(liste):
     calculation = {}
     arr = np.array(liste)
     arr = arr.reshape(3,3)
-    calculation['mean'] = [list(np.mean(arr, axis=0), list(np.mean(arr,axis=1),np.mean(arr)]
-    calculation['variance'] = [list(np.var(arr,axis=0),list(np.var(arr,axis=1),np.var(arr)]
-    calculation['standard deviation'] = [list(np.std(arr,axis=0), list(np.std(arr,axis=1), np.std(arr)]
-    calculation['max'] = [list(np.max(arr,axis=0),list(np.max(arr,axis=1),np.max(arr)]
-    calculation['min'] = [list(min(np.min(arr,axis=0),list(np.min(arr,axis=1),np.min(arr)]
-    calculation['sum'] = [list(np.sum(arr,axis=0),list(np.sum(arr,axis=1),np.sum(arr)]
+    calculation['mean'] = [list(np.mean(arr, axis=0)), list(np.mean(arr,axis=1)),np.mean(arr)]
+    calculation['variance'] = [list(np.var(arr,axis=0)),list(np.var(arr,axis=1)),np.var(arr)]
+    calculation['standard deviation'] = [list(np.std(arr,axis=0)), list(np.std(arr,axis=1)), np.std(arr)]
+    calculation['max'] = [list(np.max(arr,axis=0)),list(np.max(arr,axis=1)),np.max(arr)]
+    calculation['min'] = [list(np.min(arr,axis=0)),list(np.min(arr,axis=1)),np.min(arr)]
+    calculation['sum'] = [list(np.sum(arr,axis=0)),list(np.sum(arr,axis=1)),np.sum(arr)]
 
 
 
